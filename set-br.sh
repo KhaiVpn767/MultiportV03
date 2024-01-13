ee177a3aa710b3c1b58026110e455e4d167ead05
KhaiVpn767 <112326124+KhaiVpn767@users.noreply.github.com>
2024-01-14 03:35:24 +0800

    Add files via upload

--- set-br.sh
+++ set-br.sh
@@ -1,52 +1,19 @@
 #!/bin/bash
-# =========================================
-# Quick Setup | Script Setup Manager
-# Edition : Stable Edition V1.0
-# Auther  : KhaiVpn767
-# (C) Copyright 2022
-# =========================================
-export Server_URL="raw.githubusercontent.com/KhaiVpn767/MultiportV3/main"
-
-clear
-dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
-biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
-#########################
-MYIP=$(wget -qO- ipv4.icanhazip.com);
-MYIP=$(curl -s ipinfo.io/ip )
-MYIP=$(curl -sS ipv4.icanhazip.com)
-MYIP=$(curl -sS ifconfig.me )
-clear
-red='\e[1;31m'
-green='\e[0;32m'
-yell='\e[1;33m'
-tyblue='\e[1;36m'
-purple='\e[0;35m'
-NC='\e[0m'
-purple() { echo -e "\\033[35;1m${*}\\033[0m"; }
-tyblue() { echo -e "\\033[36;1m${*}\\033[0m"; }
-yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
-green() { echo -e "\\033[32;1m${*}\\033[0m"; }
-red() { echo -e "\\033[31;1m${*}\\033[0m"; }
-
-red='\e[1;31m'
-green='\e[0;32m'
-NC='\e[0m'
-green() { echo -e "\\033[32;1m${*}\\033[0m"; }
-red() { echo -e "\\033[31;1m${*}\\033[0m"; }
-
-#curl https://rclone.org/install.sh | bash
-apt install rclone
+# Set-Backup Installation
+# By Vinstechmy
+#-----------------------------
+curl https://rclone.org/install.sh | bash
 printf "q\n" | rclone config
-wget -O /root/.config/rclone/rclone.conf "https://${Server_URL}/rclone.conf"
+wget -O /root/.config/rclone/rclone.conf "https://raw.githubusercontent.com/vinstechmy/multiport-websocket/main/OTHERS/rclone.conf"
 git clone  https://github.com/MrMan21/wondershaper.git
 cd wondershaper
 make install
 cd
 rm -rf wondershaper
 cd /usr/bin
-wget -O backup "https://${Server_URL}/backup.sh"
-wget -O restore "https://${Server_URL}/restore.sh"
-wget -O cleaner "https://${Server_URL}/logcleaner.sh"
+wget -O backup "https://raw.githubusercontent.com/vinstechmy/multiport-websocket/main/BACKUP/backup.sh"
+wget -O restore "https://raw.githubusercontent.com/vinstechmy/multiport-websocket/main/BACKUP/restore.sh"
+wget -O cleaner "https://raw.githubusercontent.com/vinstechmy/multiport-websocket/main/BACKUP/logcleaner.sh"
 chmod +x /usr/bin/backup
 chmod +x /usr/bin/restore
 chmod +x /usr/bin/cleaner
